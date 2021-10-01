986f84d0ff1d4946005d71ff62956eb5b0ce7883
D C <me@jessicaim.com>
2021-10-01 12:20:46 -0400

    saving in api set up to filter results from that day

--- project_files/app.js
+++ project_files/app.js
@@ -3,9 +3,10 @@
 
 $.ajax(
     {
-        url: `https://api.ebird.org/v2/data/obs/US/recent/notable`,
+        url: `https://api.ebird.org/v2/data/obs/US-PA/recent/notable?detail=full&back=1`,
         type: "GET",
-        headers: { "X-ebirdApiToken": '3ucptnnv43do' }
+        headers: { "X-ebirdApiToken": '3ucptnnv43do' },
+
     }
 ).then(
     (data) => {
