e8e967cb7c0d8a4ab739516bc93bc7b94066551d
D C <me@jessicaim.com>
2021-10-02 10:52:03 -0400

    saving in working dynamic user input and results

--- project_files/app.js
+++ project_files/app.js
@@ -29,30 +29,50 @@ $(() => {
     //This is to render the data into appropriate tags
     const render = (results) => {
         console.log(results)
-        for (let i = 0; i < results.length; i++){
+        for (let i = 0; i < results.length; i++){ //results.length
             const $birdDiv = $('<div>').addClass('bird-div')
             const $birdName = $('<p>').text(results[i].comName).addClass('bird-name')
             const $birdLocation = $('<p>').text(results[i].locName).addClass('bird-location')
             const $birdCounty = $('<p>').text(results[i].subnational2Name).addClass('bird-county')
-            const $birdLearnMore = $('<button>').text('Learn More').addClass('off')
-
+            const $learnMoreButton = $('<button>').text('Learn More').addClass('off').attr('id', 'openModal')
+            const $modal = $('<div>').attr('id', 'modal')
+            const $modalContent = $('<div>').attr('id', 'modal-content')
+            const $closeModal = $('<a>').attr('id', 'close').attr('href', '#').text('Close')
+            const $modalHeader = $('<h1>').text('About this bird')
 
 
             $('#results').append($birdDiv)
             $birdDiv.append($birdName)
             $birdDiv.append($birdLocation)
             $birdDiv.append($birdCounty)
-            $birdDiv.append($birdLearnMore)
+            $birdDiv.append($learnMoreButton)
+            $birdDiv.append($modal)
+            $modal.append($modalContent)
+            $modalContent.append($closeModal)
+            $modalContent.append($modalHeader)
 
+
+            const openLearnMore = () => {
+                $modal.css('display', 'block')
+            }
+            const closeModal = () => {
+                $modal.css('display', 'none')
+            }
+
+            $learnMoreButton.on('click', openLearnMore)
+            $closeModal.on('click', closeModal)
         }
     }
 
     $('input[type="submit"]').on('click', (event) => {
         event.preventDefault()
 
+        const $userInput = $('select').val()
+        console.log($userInput);
+
         $.ajax(
             {
-                url: `https://api.ebird.org/v2/data/obs/US-PA/recent/notable?detail=full&back=1`,
+                url: `https://api.ebird.org/v2/data/obs/US-${$userInput}/recent/notable?detail=full&back=1`,
                 type: "GET",
                 headers: { "X-ebirdApiToken": '3ucptnnv43do' },
             }
@@ -60,17 +80,22 @@ $(() => {
             (data) => {
 
                 consolidate(data)
-                console.log(data);
+                // console.log(data)
+
                 // const uniqueSet = new Set(data)
                 // const newArray = [...noDataDupe]
                 // console.log(newArray);
-                // render(newArray)
+                render(data)
             },
             () => {
                 console.log('bad request');
             }
 
         )
+
+        // $('input[type="submit"]').trigger($('#results').empty())
+        // $('input[type="submit"]').trigger('reset', event)
+        $(event.currentTarget).trigger('reset', $('#results').empty())
 
 
     })
